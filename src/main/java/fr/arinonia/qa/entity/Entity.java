57334c1e6cac753d4cfd9bf76b847f0410c6cff0
Arinonia <arinonia.yt@gmail.com>
2024-03-07 12:16:29 +0100

    starting to add player images

--- src/main/java/fr/arinonia/qa/entity/Entity.java
+++ src/main/java/fr/arinonia/qa/entity/Entity.java
@@ -1,10 +1,16 @@
 package fr.arinonia.qa.entity;
 
+import java.awt.image.BufferedImage;
+
 public class Entity {
 
-    private int x;
-    private int y;
-    private int speed;
+    protected int x;
+    protected int y;
+    protected int speed;
+    protected BufferedImage up1, up2, down1, down2, left1, left2, right1, right2;
+    protected String direction;
+    private int sprite_counter = 0;
+    private int sprite_num = 1;
 
     public int getX() {
         return this.x;
@@ -28,5 +34,93 @@ public class Entity {
 
     public void setSpeed(final int speed) {
         this.speed = speed;
+    }
+
+    public BufferedImage getUp1() {
+        return this.up1;
+    }
+
+    public void setUp1(final BufferedImage up1) {
+        this.up1 = up1;
+    }
+
+    public BufferedImage getUp2() {
+        return this.up2;
+    }
+
+    public void setUp2(final BufferedImage up2) {
+        this.up2 = up2;
+    }
+
+    public BufferedImage getDown1() {
+        return this.down1;
+    }
+
+    public void setDown1(final BufferedImage down1) {
+        this.down1 = down1;
+    }
+
+    public BufferedImage getDown2() {
+        return this.down2;
+    }
+
+    public void setDown2(final BufferedImage down2) {
+        this.down2 = down2;
+    }
+
+    public BufferedImage getLeft1() {
+        return this.left1;
+    }
+
+    public void setLeft1(final BufferedImage left1) {
+        this.left1 = left1;
+    }
+
+    public BufferedImage getLeft2() {
+        return this.left2;
+    }
+
+    public void setLeft2(final BufferedImage left2) {
+        this.left2 = left2;
+    }
+
+    public BufferedImage getRight1() {
+        return this.right1;
+    }
+
+    public void setRight1(final BufferedImage right1) {
+        this.right1 = right1;
+    }
+
+    public BufferedImage getRight2() {
+        return this.right2;
+    }
+
+    public void setRight2(final BufferedImage right2) {
+        this.right2 = right2;
+    }
+
+    public String getDirection() {
+        return this.direction;
+    }
+
+    public void setDirection(final String direction) {
+        this.direction = direction;
+    }
+
+    public int getSpriteCounter() {
+        return this.sprite_counter;
+    }
+
+    public void setSpriteCounter(final int sprite_counter) {
+        this.sprite_counter = sprite_counter;
+    }
+
+    public int getSpriteNum() {
+        return this.sprite_num;
+    }
+
+    public void setSpriteNum(final int sprite_num) {
+        this.sprite_num = sprite_num;
     }
 }
--- src/main/java/fr/arinonia/qa/entity/Player.java
+++ src/main/java/fr/arinonia/qa/entity/Player.java
@@ -2,7 +2,10 @@ package fr.arinonia.qa.entity;
 
 import fr.arinonia.qa.Game;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 public class Player extends Entity {
 
@@ -13,26 +16,88 @@ public class Player extends Entity {
         this.setX(10);
         this.setY(100);
         this.setSpeed(4);
+        this.direction = "down";
+        this.getPlayerImages();
+    }
+
+    public void getPlayerImages() {
+        try {
+            this.up1 = ImageIO.read(Game.class.getResourceAsStream("/images/player/boy_up_1.png"));
+            this.up2 = ImageIO.read(Game.class.getResourceAsStream("/images/player/boy_up_2.png"));
+            this.down1 = ImageIO.read(Game.class.getResourceAsStream("/images/player/boy_down_1.png"));
+            this.down2 = ImageIO.read(Game.class.getResourceAsStream("/images/player/boy_down_2.png"));
+            this.left1 = ImageIO.read(Game.class.getResourceAsStream("/images/player/boy_left_1.png"));
+            this.left2 = ImageIO.read(Game.class.getResourceAsStream("/images/player/boy_left_2.png"));
+            this.right1 = ImageIO.read(Game.class.getResourceAsStream("/images/player/boy_right_1.png"));
+            this.right2 = ImageIO.read(Game.class.getResourceAsStream("/images/player/boy_right_1.png"));
+
+        } catch (final IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void update() {
         if (this.game.getKey_handler().isUp()) {
+            this.direction = "up";
             this.setY(this.getY() - this.getSpeed());
         }
         if (this.game.getKey_handler().isDown()) {
+            this.direction = "down";
             this.setY(this.getY() + this.getSpeed());
         }
         if (this.game.getKey_handler().isLeft()) {
+            this.direction = "left";
             this.setX(this.getX() - this.getSpeed());
         }
         if (this.game.getKey_handler().isRight()) {
+            this.direction = "right";
             this.setX(this.getX() + this.getSpeed());
+        }
+        this.setSpriteCounter(this.getSpriteCounter() + 1);
+
+        if (this.getSpriteNum() > 10) {
+            if (this.getSpriteNum() == 1) {
+                this.setSpriteNum(2);
+            } else if (this.getSpriteNum() == 2) {
+                this.setSpriteNum(1);
+            }
+            this.setSpriteCounter(0);
         }
     }
 
     public void draw(final Graphics2D g2) {
-        g2.setColor(Color.WHITE);
-            g2.fillRect(this.getX(), this.getY(), this.game.getGamePanel().getTileSize(),
-                    this.game.getGamePanel().getTileSize());
+        BufferedImage image = null;
+        switch (this.direction) {
+            case "up":
+                if (this.getSpriteNum() == 1) {
+                    image = this.up1;
+                } else if (this.getSpriteNum() == 2) {
+                    image = this.up2;
+                }
+                break;
+            case "down":
+                if (this.getSpriteNum() == 1) {
+                    image = this.down1;
+                } else if (this.getSpriteNum() == 2) {
+                    image = this.down2;
+                }
+                break;
+            case "left":
+                if (this.getSpriteNum() == 1) {
+                    image = this.left1;
+                } else if (this.getSpriteNum() == 2) {
+                    image = this.left2;
+                }
+                break;
+            case "right":
+                if (this.getSpriteNum() == 1) {
+                    image = this.right1;
+                } else if (this.getSpriteNum() == 2) {
+                    image = this.right2;
+                }
+                break;
+        }
+        g2.drawImage(image, this.x, this.y, this.game.getGamePanel().getTileSize(),
+                this.game.getGamePanel().getTileSize(), null);
     }
 }
